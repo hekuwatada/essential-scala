6cb463f3921a3767b167aa6a453ba24ad175a0f6
Heku Watada <heku.watada@gmail.com>
2019-03-05 07:16:35 +0000

    C.5.2.3: Restored sum, product, length, double implementation with pattern matching for comparison.

--- src/main/scala/me/namake/scala/IntList.scala
+++ src/main/scala/me/namake/scala/IntList.scala
@@ -3,11 +3,31 @@ package me.namake.scala
 sealed trait IntList {
   def sum: Int = fold(0)(_ + _)
 
+  def sum2: Int = this match {
+    case IntListEnd => 0
+    case IntListPair(head, tail) => head + tail.sum2
+  }
+
   def product: Int = fold(1)(_ * _)
+
+  def product2: Int = this match {
+    case IntListEnd => 1
+    case IntListPair(head, tail) => head * tail.product2
+  }
 
   def length: Int = fold(0)((_, tail) => 1 + tail)
 
+  def length2: Int = this match {
+    case IntListEnd => 0
+    case IntListPair(_, tail) => 1 + tail.length2
+  }
+
   def double: IntList = fold(IntListEnd: IntList)((head, tail) => IntListPair(head * 2, tail))
+
+  def double2: IntList = this match {
+    case IntListEnd => IntListEnd
+    case IntListPair(head, tail) => IntListPair(head * 2, tail.double2)
+  }
 
   //TODO: make it tail recursive
   def fold[A](end: A)(f: (Int, A) => A): A =
--- src/test/scala/me/namake/scala/IntListSpec.scala
+++ src/test/scala/me/namake/scala/IntListSpec.scala
@@ -4,33 +4,68 @@ import org.scalatest.{FunSpec, Matchers}
 
 class IntListSpec extends FunSpec with Matchers {
 
-  describe("sum") {
-    it("returns sum") {
-      IntListEnd.sum shouldBe 0
-      IntListPair(1, IntListPair(2, IntListPair(3, IntListEnd))).sum shouldBe 6
+  describe("implemented with fold") {
+    describe("sum") {
+      it("returns sum") {
+        IntListEnd.sum shouldBe 0
+        IntListPair(1, IntListPair(2, IntListPair(3, IntListEnd))).sum shouldBe 6
+      }
+    }
+
+    describe("length") {
+      it("returns length of list") {
+        IntListEnd.length shouldBe 0
+        IntListPair(1, IntListEnd).length shouldBe 1
+        IntListPair(1, IntListPair(2, IntListPair(3, IntListPair(4, IntListEnd)))).length shouldBe 4
+      }
+    }
+
+    describe("product") {
+      it("returns product") {
+        IntListEnd.product shouldBe 1
+        IntListPair(3, IntListPair(2, IntListPair(5, IntListEnd))).product shouldBe 30
+      }
+    }
+
+    describe("double") {
+      it("doubles elements") {
+        IntListEnd.double shouldBe IntListEnd
+        IntListPair(1, IntListEnd).double shouldBe IntListPair(2, IntListEnd)
+        IntListPair(3, IntListPair(2, IntListPair(5, IntListEnd))).double shouldBe IntListPair(6, IntListPair(4, IntListPair(10, IntListEnd)))
+      }
     }
   }
 
-  describe("length") {
-    it("returns length of list") {
-      IntListEnd.length shouldBe 0
-      IntListPair(1, IntListEnd).length shouldBe 1
-      IntListPair(1, IntListPair(2, IntListPair(3, IntListPair(4, IntListEnd)))).length shouldBe 4
+  //TODO: remove code duplication
+  describe("implemented with pattern matching") {
+    describe("sum") {
+      it("returns sum") {
+        IntListEnd.sum2 shouldBe 0
+        IntListPair(1, IntListPair(2, IntListPair(3, IntListEnd))).sum2 shouldBe 6
+      }
     }
-  }
 
-  describe("product") {
-    it("returns product") {
-      IntListEnd.product shouldBe 1
-      IntListPair(3, IntListPair(2, IntListPair(5, IntListEnd))).product shouldBe 30
+    describe("length") {
+      it("returns length of list") {
+        IntListEnd.length2 shouldBe 0
+        IntListPair(1, IntListEnd).length2 shouldBe 1
+        IntListPair(1, IntListPair(2, IntListPair(3, IntListPair(4, IntListEnd)))).length2 shouldBe 4
+      }
     }
-  }
 
-  describe("double") {
-    it("doubles elements") {
-      IntListEnd.double shouldBe IntListEnd
-      IntListPair(1, IntListEnd).double shouldBe IntListPair(2, IntListEnd)
-      IntListPair(3, IntListPair(2, IntListPair(5, IntListEnd))).double shouldBe IntListPair(6, IntListPair(4, IntListPair(10, IntListEnd)))
+    describe("product") {
+      it("returns product") {
+        IntListEnd.product2 shouldBe 1
+        IntListPair(3, IntListPair(2, IntListPair(5, IntListEnd))).product2 shouldBe 30
+      }
+    }
+
+    describe("double") {
+      it("doubles elements") {
+        IntListEnd.double2 shouldBe IntListEnd
+        IntListPair(1, IntListEnd).double2 shouldBe IntListPair(2, IntListEnd)
+        IntListPair(3, IntListPair(2, IntListPair(5, IntListEnd))).double2 shouldBe IntListPair(6, IntListPair(4, IntListPair(10, IntListEnd)))
+      }
     }
   }
 }
