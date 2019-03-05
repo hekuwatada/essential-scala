c7f5105790b617a9a21ae7e1e6077fd850af761e
Heku Watada <heku.watada@gmail.com>
2019-03-05 07:03:09 +0000

    C.5.2.3: Renamed subtypes of IntList in preparation for adding generic list.

--- src/main/scala/me/namake/scala/IntList.scala
+++ src/main/scala/me/namake/scala/IntList.scala
@@ -7,15 +7,15 @@ sealed trait IntList {
 
   def length: Int = fold(0)((_, tail) => 1 + tail)
 
-  def double: IntList = fold(End: IntList)((head, tail) => Pair(head * 2, tail))
+  def double: IntList = fold(IntListEnd: IntList)((head, tail) => IntListPair(head * 2, tail))
 
   //TODO: make it tail recursive
   def fold[A](end: A)(f: (Int, A) => A): A =
     this match {
-      case End => end
-      case Pair(head, tail) => f(head, tail.fold(end)(f))
+      case IntListEnd => end
+      case IntListPair(head, tail) => f(head, tail.fold(end)(f))
     }
 }
 
-case object End extends IntList
-final case class Pair(head: Int, tail: IntList) extends IntList
+case object IntListEnd extends IntList
+final case class IntListPair(head: Int, tail: IntList) extends IntList
--- src/test/scala/me/namake/scala/IntListSpec.scala
+++ src/test/scala/me/namake/scala/IntListSpec.scala
@@ -6,31 +6,31 @@ class IntListSpec extends FunSpec with Matchers {
 
   describe("sum") {
     it("returns sum") {
-      End.sum shouldBe 0
-      Pair(1, Pair(2, Pair(3, End))).sum shouldBe 6
+      IntListEnd.sum shouldBe 0
+      IntListPair(1, IntListPair(2, IntListPair(3, IntListEnd))).sum shouldBe 6
     }
   }
 
   describe("length") {
     it("returns length of list") {
-      End.length shouldBe 0
-      Pair(1, End).length shouldBe 1
-      Pair(1, Pair(2, Pair(3, Pair(4, End)))).length shouldBe 4
+      IntListEnd.length shouldBe 0
+      IntListPair(1, IntListEnd).length shouldBe 1
+      IntListPair(1, IntListPair(2, IntListPair(3, IntListPair(4, IntListEnd)))).length shouldBe 4
     }
   }
 
   describe("product") {
     it("returns product") {
-      End.product shouldBe 1
-      Pair(3, Pair(2, Pair(5, End))).product shouldBe 30
+      IntListEnd.product shouldBe 1
+      IntListPair(3, IntListPair(2, IntListPair(5, IntListEnd))).product shouldBe 30
     }
   }
 
   describe("double") {
     it("doubles elements") {
-      End.double shouldBe End
-      Pair(1, End).double shouldBe Pair(2, End)
-      Pair(3, Pair(2, Pair(5, End))).double shouldBe Pair(6, Pair(4, Pair(10, End)))
+      IntListEnd.double shouldBe IntListEnd
+      IntListPair(1, IntListEnd).double shouldBe IntListPair(2, IntListEnd)
+      IntListPair(3, IntListPair(2, IntListPair(5, IntListEnd))).double shouldBe IntListPair(6, IntListPair(4, IntListPair(10, IntListEnd)))
     }
   }
 }
