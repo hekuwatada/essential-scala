54b116c801319acb520cb6982151dc747f8e5d31
Heku Watada <heku.watada@gmail.com>
2019-03-05 04:26:40 +0000

    C.5.2.3: Added double without fold.

--- src/main/scala/me/namake/scala/IntList.scala
+++ src/main/scala/me/namake/scala/IntList.scala
@@ -3,9 +3,15 @@ package me.namake.scala
 sealed trait IntList {
   def sum: Int = fold(0)((x, y) => x + y)
 
+  def product: Int = fold(1)((x, y) => x * y)
+
   def length: Int = fold(0)((_, y) => 1 + y)
 
-  def product: Int = fold(1)((x, y) => x * y)
+  def double: IntList =
+    this match {
+      case End => End
+      case Pair(head, tail) => Pair(head * 2, tail.double)
+    }
 
   def fold(end: Int)(f: (Int, Int) => Int): Int =
     this match {
--- src/test/scala/me/namake/scala/IntListSpec.scala
+++ src/test/scala/me/namake/scala/IntListSpec.scala
@@ -25,4 +25,12 @@ class IntListSpec extends FunSpec with Matchers {
       Pair(3, Pair(2, Pair(5, End))).product shouldBe 30
     }
   }
+
+  describe("double") {
+    it("doubles elements") {
+      End.double shouldBe End
+      Pair(1, End).double shouldBe Pair(2, End)
+      Pair(3, Pair(2, Pair(5, End))).double shouldBe Pair(6, Pair(4, Pair(10, End)))
+    }
+  }
 }
