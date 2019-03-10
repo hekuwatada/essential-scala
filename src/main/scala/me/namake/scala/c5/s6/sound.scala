8af447441df6e54c177709c9ce9af9c242ee67d3
Heku Watada <heku.watada@gmail.com>
2019-03-10 10:48:13 +0000

    C.5.6.6.1: Added TODO for type parameter with variant flip.

--- src/main/scala/me/namake/scala/c5/s6/sound.scala
+++ src/main/scala/me/namake/scala/c5/s6/sound.scala
@@ -3,6 +3,7 @@ package me.namake.scala.c5.s6
 import me.namake.scala.misc.{Cat, CatSound}
 
 object sound {
+  //TODO: add example with type parameter
   def groom(groomer: Cat => CatSound): CatSound = {
     val blackCat: Cat = new Cat { val name = "black" }
     groomer(blackCat)
