a46aab579ff64dae7190d5b81c306fd9ae05966b
Heku Watada <heku.watada@gmail.com>
2019-03-11 07:25:27 +0000

    C.6.2.7.2: Implemented min() with foldLeft for Seq. This solution returns max value when collection is empty.

--- src/main/scala/me/namake/scala/c6/seqAddon.scala
+++ src/main/scala/me/namake/scala/c6/seqAddon.scala
@@ -3,5 +3,7 @@ package me.namake.scala.c6
 //TODO: implement implicit class for Seq
 object seqAddon {
 
-  def min(col: Seq[Int]): Option[Int] = col.sorted.headOption
+  def minOption(col: Seq[Int]): Option[Int] = col.sorted.headOption
+
+  def min(col: Seq[Int]): Int = col.foldLeft(Int.MaxValue)(Math.min)
 }
--- src/test/scala/me/namake/scala/c6/seqAddonSpec.scala
+++ src/test/scala/me/namake/scala/c6/seqAddonSpec.scala
@@ -3,11 +3,22 @@ package me.namake.scala.c6
 import org.scalatest.{FunSpec, Matchers}
 
 class seqAddonSpec extends FunSpec with Matchers {
+  describe("minOption()") {
+    it("returns the smallest integer") {
+      seqAddon.minOption(Seq(3, 2, 9)) shouldBe Some(2)
+      seqAddon.minOption(Seq(1)) shouldBe Some(1)
+      seqAddon.minOption(Seq()) shouldBe None
+    }
+  }
+
   describe("min()") {
     it("returns the smallest integer") {
-      seqAddon.min(Seq(3, 2, 9)) shouldBe Some(2)
-      seqAddon.min(Seq(1)) shouldBe Some(1)
-      seqAddon.min(Seq()) shouldBe None
+      seqAddon.min(Seq(4, 2, 9)) shouldBe 2
+      seqAddon.min(Seq(5)) shouldBe 5
+    }
+
+    it("returns max integer when collection is empty") {
+      seqAddon.min(Seq()) shouldBe Int.MaxValue
     }
   }
 }
