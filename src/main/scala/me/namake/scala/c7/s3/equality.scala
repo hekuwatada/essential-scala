d3ed23a182f611fa5ab5845b186b20a74b3599a8
Heku Watada <heku.watada@gmail.com>
2019-03-12 09:06:56 +0000

    C.7.4.4.1: Changed implicit val to implicit object for lazy evaluation.

--- src/main/scala/me/namake/scala/c7/s3/equality.scala
+++ src/main/scala/me/namake/scala/c7/s3/equality.scala
@@ -1,13 +1,17 @@
 package me.namake.scala.c7.s3
 
 object equality {
-  // Single Abstract Method
   object personEqualityByEmail {
-    implicit val eq: Equal[Person] = (x, y) => x.email == y.email
+    implicit object eq extends Equal[Person] {
+      override def equal(x: Person, y: Person): Boolean = x.email == y.email
+    }
   }
 
   object personEqualityByNameEmail {
-    implicit val eq: Equal[Person] = (x, y) => x.name == y.name && x.email == y.email
+    implicit object eq extends Equal[Person] {
+      override def equal(x: Person, y: Person): Boolean =
+        x.name == y.name && x.email == y.email
+    }
   }
 
   object Eq {
