0691a2488613b97c6503d869f3a0282c13179459
Heku Watada <heku.watada@gmail.com>
2019-03-12 06:59:20 +0000

    C.2.5.1: Added default ordering defined in companion object.

--- src/main/scala/me/namake/scala/c7/order/Order.scala
+++ src/main/scala/me/namake/scala/c7/order/Order.scala
@@ -3,3 +3,7 @@ package me.namake.scala.c7.order
 final case class Order(units: Int, unitPrice: Double) {
   val totalPrice: Double = units * unitPrice
 }
+
+object Order {
+  implicit val ordering: Ordering[Order] = implicits.orderByTotalPrice.ordering
+}
--- src/test/scala/me/namake/scala/c7/order/OrderSpec.scala
+++ src/test/scala/me/namake/scala/c7/order/OrderSpec.scala
@@ -7,8 +7,7 @@ class OrderSpec extends FunSpec with Matchers {
   describe("ordering") {
     val orders = Seq(Order(5, 9.5), Order(2, 20.0), Order(1, 100))
 
-    it("orders by total price") {
-      import implicits.orderByTotalPrice._
+    it("orders by total price by default") {
       orders.sorted shouldBe Seq(Order(2, 20.0), Order(5, 9.5), Order(1, 100))
     }
 
