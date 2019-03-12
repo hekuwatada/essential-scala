abf1c30d90f235020909bade72aaf70aa0185423
Heku Watada <heku.watada@gmail.com>
2019-03-12 08:50:32 +0000

    C.7.4.4.1: Implemented object Eq with implicit Equal[A] parameter.

--- src/main/scala/me/namake/scala/c7/s3/equality.scala
+++ src/main/scala/me/namake/scala/c7/s3/equality.scala
@@ -3,10 +3,14 @@ package me.namake.scala.c7.s3
 object equality {
   // Single Abstract Method
   object personEqualityByEmail {
-    val equality: Equal[Person] = (x, y) => x.email == y.email
+    implicit val eq: Equal[Person] = (x, y) => x.email == y.email
   }
 
   object personEqualityByNameEmail {
-    val equality: Equal[Person] = (x, y) => x.name == y.name && x.email == y.email
+    implicit val eq: Equal[Person] = (x, y) => x.name == y.name && x.email == y.email
+  }
+
+  object Eq {
+    def apply[A](x: A, y: A)(implicit eq: Equal[A]): Boolean = eq.equal(x, y)
   }
 }
--- src/test/scala/me/namake/scala/c7/s3/equalitySpec.scala
+++ src/test/scala/me/namake/scala/c7/s3/equalitySpec.scala
@@ -4,15 +4,37 @@ import org.scalatest.{FunSpec, Matchers}
 
 class equalitySpec extends FunSpec with Matchers {
 
+  private def testEmailEquality(eq: (Person, Person) => Boolean): Unit = {
+    val p1 = Person("name", "email")
+    val p2 = p1.copy(name = "another name")
+    val p3 = p1.copy(email = "another email")
+
+    eq(p1, p1) shouldBe true
+    eq(p1, p2) shouldBe true
+    eq(p1, p3) shouldBe false
+  }
+
+  private def testNameEmailEquality(eq: (Person, Person) => Boolean): Unit = {
+    val p1 = Person("name", "email")
+    val p2 = p1.copy(name = "another name")
+    val p3 = p1.copy(email = "another email")
+    val p4 = Person("name", "email")
+
+    eq(p1, p1) shouldBe true
+    eq(p1, p4) shouldBe true
+    eq(p1, p2) shouldBe false
+    eq(p1, p3) shouldBe false
+  }
+
   describe("equality for Person") {
     it("considers identical email addresses as equal") {
       val p1 = Person("name", "email")
       val p2 = p1.copy(name = "another name")
       val p3 = p1.copy(email = "another email")
 
-      equality.personEqualityByEmail.equality.equal(p1, p1) shouldBe true
-      equality.personEqualityByEmail.equality.equal(p1, p2) shouldBe true
-      equality.personEqualityByEmail.equality.equal(p1, p3) shouldBe false
+      equality.personEqualityByEmail.eq.equal(p1, p1) shouldBe true
+      equality.personEqualityByEmail.eq.equal(p1, p2) shouldBe true
+      equality.personEqualityByEmail.eq.equal(p1, p3) shouldBe false
     }
 
     it("considers identical name and email addresses as equal") {
@@ -21,10 +43,38 @@ class equalitySpec extends FunSpec with Matchers {
       val p3 = p1.copy(email = "another email")
       val p4 = Person("name", "email")
 
-      equality.personEqualityByNameEmail.equality.equal(p1, p1) shouldBe true
-      equality.personEqualityByNameEmail.equality.equal(p1, p4) shouldBe true
-      equality.personEqualityByNameEmail.equality.equal(p1, p2) shouldBe false
-      equality.personEqualityByNameEmail.equality.equal(p1, p3) shouldBe false
+      equality.personEqualityByNameEmail.eq.equal(p1, p1) shouldBe true
+      equality.personEqualityByNameEmail.eq.equal(p1, p4) shouldBe true
+      equality.personEqualityByNameEmail.eq.equal(p1, p2) shouldBe false
+      equality.personEqualityByNameEmail.eq.equal(p1, p3) shouldBe false
+    }
+  }
+
+  describe("type class interface pattern") {
+    it("considers identical email addresses as equal") {
+      val p1 = Person("name", "email")
+      val p2 = p1.copy(name = "another name")
+      val p3 = p1.copy(email = "another email")
+
+      import equality.personEqualityByEmail.{eq => eqF}
+
+      equality.Eq(p1, p1) shouldBe true
+      equality.Eq(p1, p2) shouldBe true
+      equality.Eq(p1, p3) shouldBe false
+    }
+
+    it("considers identical name and email addresses as equal") {
+      val p1 = Person("name", "email")
+      val p2 = p1.copy(name = "another name")
+      val p3 = p1.copy(email = "another email")
+      val p4 = Person("name", "email")
+
+      import equality.personEqualityByNameEmail.{eq => eqF}
+
+      equality.Eq(p1, p1) shouldBe true
+      equality.Eq(p1, p4) shouldBe true
+      equality.Eq(p1, p2) shouldBe false
+      equality.Eq(p1, p3) shouldBe false
     }
   }
 }
