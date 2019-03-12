2b23b564167539ea26470df283b43c5f5f05cab2
Heku Watada <heku.watada@gmail.com>
2019-03-12 05:44:31 +0000

    C.7.2: Changed implicit scope from local to companion object.

--- src/main/scala/me/namake/scala/c7/Rational.scala
+++ src/main/scala/me/namake/scala/c7/Rational.scala
@@ -3,7 +3,7 @@ package me.namake.scala.c7
 final case class Rational(numerator: Int, denominator: Int)
 
 object Rational {
-  val ordering: Ordering[Rational] = Ordering.fromLessThan((x, y) =>
+  implicit val ordering: Ordering[Rational] = Ordering.fromLessThan((x, y) =>
     x.numerator * y.denominator < y.numerator * x.denominator
   )
 }
--- src/test/scala/me/namake/scala/c7/RationalSpec.scala
+++ src/test/scala/me/namake/scala/c7/RationalSpec.scala
@@ -6,7 +6,6 @@ class RationalSpec extends FunSpec with Matchers {
 
   describe("ordering") {
     it("orders from smallest to largest") {
-      implicit val ordering: Ordering[Rational] = Rational.ordering
       List(Rational(1, 2), Rational(3, 4), Rational(1, 3)).sorted shouldBe
         List(Rational(1, 3), Rational(1, 2), Rational(3, 4))
     }
