58d8ccd66944b18e7278ad30cf89ad75c4616d7e
Heku Watada <heku.watada@gmail.com>
2019-03-12 07:42:15 +0000

    C.7.3.4.1: Refactored equality with Single Abstract Method.

--- src/main/scala/me/namake/scala/c7/s3/equality.scala
+++ src/main/scala/me/namake/scala/c7/s3/equality.scala
@@ -1,7 +1,6 @@
 package me.namake.scala.c7.s3
 
 object equality {
-  val personEquality: Equal[Person] = new Equal[Person] {
-    override def equal(x: Person, y: Person): Boolean = x.email == y.email
-  }
+  // Single Abstract Method
+  val personEquality: Equal[Person] = (x, y) => x.email == y.email
 }
