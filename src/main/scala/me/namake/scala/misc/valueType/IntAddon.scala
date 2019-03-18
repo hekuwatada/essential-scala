bbc0b6055e354a530f2dab7f488d394c69a102bb
Heku Watada <heku.watada@gmail.com>
2019-03-18 14:48:51 +0000

    Value Type - added example of implicit value class whoes method will be called as a static method.

--- src/main/scala/me/namake/scala/misc/valueType/IntAddon.scala
+++ src/main/scala/me/namake/scala/misc/valueType/IntAddon.scala
@@ -1,7 +1,17 @@
 package me.namake.scala.misc.valueType
 
 object IntAddon {
+  //@see https://docs.scala-lang.org/overviews/core/implicit-classes.html
   implicit class ExtendedInt(value: Int) {
+    println("ExtendedInt instantiated")
+
     def toHexStr: String = java.lang.Integer.toHexString(value)
+  }
+
+  //@see https://docs.scala-lang.org/overviews/core/value-classes.html
+  implicit class AllocationFreeExtendedInt(val value: Int) extends AnyVal {
+    //NOTE: 123.toStr will be called as a static method, as opposed to
+    // a new instance of AllocationFreeExtendedInt class created.
+    def toStr: String = value.toString
   }
 }
--- src/test/scala/me/namake/scala/misc/valueType/IntAddonSpec.scala
+++ src/test/scala/me/namake/scala/misc/valueType/IntAddonSpec.scala
@@ -4,10 +4,17 @@ import org.scalatest.{FunSpec, Matchers}
 
 class IntAddonSpec extends FunSpec with Matchers {
   describe("implicit class") {
-    it("enriches Int with method") {
+    it("enriches Int with method - plain") {
       import me.namake.scala.misc.valueType.IntAddon._
 
       42.toHexStr shouldBe "2a"
+    }
+
+    it("enriched Int as value class with AnyVal") {
+      import me.namake.scala.misc.valueType.IntAddon._
+
+      // toStr is a static method call
+      42.toStr shouldBe "42"
     }
   }
 }
